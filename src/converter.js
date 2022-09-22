8919a9bc8339b63e3817d6cd7a39a3895fc8f14f
ROL - Rendimento Online <filipe_alvesoliveira@hotmail.com>
2022-09-21 23:54:10 -0300

    update convertions

--- src/converter.js
+++ src/converter.js
@@ -1,14 +1,24 @@
-function Converter(){
-    var valorElemento = document.getElementById("valor");
-    var valor = valorElemento.value;
-    
-    var valorEmDolarNumerico = parseFloat(valor);
-    console.log(valorElemento);
-    
-    var valorEmReal = valorEmDolarNumerico * 5;
-    console.log(valorEmReal);
-    
-    var elementoValorConvertido = document.getElementById("valorConvertido")
-    var valorConvertido = "O resultado em real é R$ " + valorEmReal;
-    elementoValorConvertido.innerHTML = valorConvertido;
-  }
+function Converter() {
+  var valorElemento = document.getElementById("valor");
+  var valor = valorElemento.value;
+
+  var valorEmDolarNumerico = parseFloat(valor);
+  console.log(valorElemento);
+
+  var valorEmEuroNumerico = parseFloat(valor);
+  console.log(valorElemento);
+
+  var valorEmReal = valorEmDolarNumerico * 5.17;
+  console.log(valorEmReal);
+
+  var valorEmDolar = valorEmEuroNumerico * 5.03;
+  console.log(valorEmReal);
+
+  var elementoValorConvertido = document.getElementById("valorConvertido");
+  var valorConvertido = "O resultado em real é R$ " + valorEmReal;
+  elementoValorConvertido.innerHTML = valorConvertido;
+
+  var elementoValorConvertido = document.getElementById("valorConvertido");
+  var valorConvertido2 = "O resultado em real é R$ " + valorEmDolar;
+  elementoValorConvertido.innerHTML = valorConvertido2;
+}
